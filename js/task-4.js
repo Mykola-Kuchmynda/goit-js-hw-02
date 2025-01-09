246407ab5ba43335b92bd55bb147c09dc049693c
Mykola Kuchmynda <mykola.kuchmynda@gmail.com>
2025-01-09 09:29:25 +0200

    change task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,26 +1,19 @@
 function getShippingCost(country) {
     let checkCountry = country.toLowerCase();
     let price;
-    if (checkCountry === "australia") {
-        price = 170;
-    } else if (checkCountry === "china") {
-        price = 100;
-    } else if (checkCountry === "chile") {
-        price = 250;
-    } else if (checkCountry === "jamaica") {
-        price = 120;
-    };
     switch (checkCountry) {
-        case "australia":
-        case "china":
-        case "chile":
-        case "jamaica":
-            return `"Shipping to ${country} will cost ${price} credits"`;
-            break;     
-        default:
-            return `"Sorry, there is no delivery to your country"`;
+        case "australia": price = 170;            
+            break;
+        case "china": price = 100;
+            break;
+        case "chile": price = 250;
+            break;
+        case "jamaica": price = 120;
+            break;    
+        default: return `"Sorry, there is no delivery to your country"`;
     }
-}
+        return `"Shipping to ${country} will cost ${price} credits"`;
+    }
 console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
 console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
